e45abeb2b4ebdf5c40fc2ce9666a484c24d8c2e0
Imran Hussain <imranhussain399@gmail.com>
2024-01-19 22:14:51 +0500

    Due date formatting using luxon library

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { DateTime } = require("luxon");
 const { Schema } = mongoose;
 const BookInstanceSchema = new Schema({
   book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
@@ -15,8 +15,14 @@ const BookInstanceSchema = new Schema({
 
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
+  // We don't use an arrow function as we'll need the "this" object
   return `/catalog/bookinstance/${this._id}`;
+});
+
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(
+    DateTime.DATETIME_MED
+  );
 });
 
 // Export model
