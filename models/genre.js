3538814f9efbb09607179ef191d75e93f8e72621
Imran Hussain <imranhussain399@gmail.com>
2024-01-20 11:38:43 +0500

    Implement url virtual property to genre Schema

--- models/genre.js
+++ models/genre.js
@@ -6,6 +6,8 @@ const GenreSchema = new Schema({
   name: { type: String, required: true, minLength: 3, maxLength: 100 },
 });
 
-GenreSchema.virtual("url");
+GenreSchema.virtual("url").get(function () {
+  return `/catalog/genre/${this._id}`;
+});
 
 module.exports = mongoose.model("Genre", GenreSchema);
