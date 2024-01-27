a6f741a93e5f38f71bd4d6b2894e21ca8464a4fd
Imran Hussain <imranhussain399@gmail.com>
2024-01-27 12:01:09 +0500

    Implement author delete route handler

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,28 +1,28 @@
 const Author = require("../models/author");
 const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
-const library_db = require("../library_db");
+
 const { body, validationResult } = require("express-validator");
 const author = require("../models/author");
 
 exports.author_list = asyncHandler(async (req, res, next) => {
-  await library_db.connect();
+
 
   const allAuthors = await Author.find({}).exec();
 
   res.render("author_list", { title: "Author List", author_list: allAuthors });
 
-  await library_db.close();
+
 });
 
 exports.author_detail = asyncHandler(async (req, res, next) => {
-  await library_db.connect();
+
   const [author, author_books] = await Promise.all([
     Author.findById(req.params.id),
     Book.find({ author: req.params.id }),
   ]);
   res.render("author_detail", { author, author_books });
-  await library_db.close();
+
 });
 
 exports.author_create_get = asyncHandler(async (req, res, next) => {
@@ -52,7 +52,7 @@ exports.author_create_post = [
     .isISO8601()
     .toDate(),
   asyncHandler(async (req, res, next) => {
-    await library_db.connect();
+
     // Extract errors
     const errors = validationResult(req);
     // Create Author model with data received
@@ -74,16 +74,27 @@ exports.author_create_post = [
       await author.save();
       res.redirect(author.url);
     }
-    await library_db.close();
+
   }),
 ];
 
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOt implemented: author delete get");
+
+
+  // Find details of author being deleted and his associated books
+  const [author, authorBooks] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary")
+      .sort({ title: 1 })
+      .exec(),
+  ]);
+  res.render("author_delete", { author, authorBooks });
+
 });
 
 exports.author_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("Not Implemented: author delete post");
+  await Author.findOneAndDelete({ _id: req.params.id }).exec();
+  res.redirect("/catalog/authors");
 });
 
 exports.author_update_get = asyncHandler(async (req, res, next) => {
