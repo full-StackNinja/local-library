6a6af504ba710d81ceb5cb6b5da57b4800a441de
Imran Hussain <imranhussain399@gmail.com>
2024-02-12 22:22:29 +0500

    Modify library_db module to export database module instead of mongoose

--- library_db.js
+++ library_db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-(async function () {
+const library_db = async function () {
   await mongoose.connect(process.env.DATABASE_URL);
-})();
+  console.log("Connected to the database")
+}
 
-module.exports = mongoose;
+module.exports = library_db;
